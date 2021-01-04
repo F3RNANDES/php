8ff787d45394c5ea8d92f7862712eb19ccbbbf6d
Iago Nascimento <iagoxiago2@gmail.com>
2021-01-04 09:14:34 -0300

    exemplos strings

---
+++ strings/exemplo-03.php
@@ -0,0 +1,7 @@
+<?php
+// str_replace , subistituir string
+$game = "league of legends";
+$game = str_replace("e","3",$empresa);
+$game = str_replace("o","0",$empresa);
+echo $empresa;
+?>
---
+++ strings/exemplo-04.php
@@ -0,0 +1,11 @@
+<?php
+$frase = "a repetição é a mãe da retenção.";
+$palavra = "mãe";
+$q = strpos($frase,"mãe");
+$texto = substr($frase,0,$q);
+var_dump($texto);
+$texto2 = substr($frase,$q + strlen($palavra),stroen($frase));
+echo "<br>";
+var_dump($texto2);
+
+?>
